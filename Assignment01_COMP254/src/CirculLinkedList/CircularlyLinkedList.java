6d83f78e292e67b7145f223d044b053aa49cd8c9
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-01-29 13:45:17 -0500

    added/modified data

--- Assignment01_COMP254/src/CirculLinkedList/CircularlyLinkedList.java
+++ Assignment01_COMP254/src/CirculLinkedList/CircularlyLinkedList.java
@@ -1,5 +1,51 @@
 package CirculLinkedList;
 
-public class CircularlyLinkedList {
+public class CircularlyLinkedList<E> {
 
-}
+	private static class Node<E> {
+		private E element;
+		private Node<E> next;
+
+		public Node(E e, Node<E> n) {
+			element = e;
+			next = n;
+		}
+
+		public E getElement() {
+			return element;
+
+		}
+
+		public Node<E> getNext() {
+			return next;
+		}
+
+		public void setSet(Node<E> n) {
+			next = n;
+		}
+	}
+			//nested node class
+			//instance variables
+			private Node<E> tail=null;
+			private int size=0;
+			
+			//constructor with empty list
+			public CircularlyLinkedList( ) 
+			{
+				
+			}
+			public int size() {
+				return size;
+				
+			}
+			public boolean isEmpty() {
+				return size==0;
+			}
+			public E first() {
+				if (isEmpty())
+					return null;
+				return tail.getNext().getElement();
+				
+			}
+		}
+	
