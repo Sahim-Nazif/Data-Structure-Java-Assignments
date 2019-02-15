b1837f5055504b9e8721893f684a692d3eda238b
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-02-14 19:40:01 -0500

    declared variables

--- Assignment02_COMP254/src/Uniqueness/Uniqueness.java
+++ Assignment02_COMP254/src/Uniqueness/Uniqueness.java
@@ -1,9 +1,51 @@
 package Uniqueness;
 
+import java.util.Arrays;
+
 public class Uniqueness {
+
+	public static boolean unique1(int[] data) {
+		int n = data.length;
+		for (int j = 0; j < n - 1; j++)
+			for (int k = j + 1; k < n; k++)
+				if (data[j] == data[k])
+					return false; // found duplicate pair
+		return true; // if we reach this, elements are unique
+	}
+
+	/** Returns true if there are no duplicate elements in the array. */
+	public static boolean unique2(int[] data) {
+		int n = data.length;
+		int[] temp = Arrays.copyOf(data, n); // make copy of data
+		Arrays.sort(temp); // and sort the copy
+		for (int j = 0; j < n - 1; j++)
+			if (temp[j] == temp[j + 1]) // check neighboring entries
+				return false; // found duplicate pair
+		return true; // if we reach this, elements are unique
+	}
 
 	public static void main(String[] args) {
 		
+		long startTime, endTime,totalTime;
+		//checking the time
+		int amountArray=(int)Math.pow(2, 20);
+		int data[]=new int [amountArray];
+		
+		int i;
+		for (i=0; i<amountArray; i++) {
+			data[i]=amountArray-1+1;
+			
+			
+		}
+		
+		i=1;
+		totalTime=0;
+		
+		while(totalTime/Math.pow(10, 6)<1) {
+			
+		}
+			
+	
 
 	}
 
