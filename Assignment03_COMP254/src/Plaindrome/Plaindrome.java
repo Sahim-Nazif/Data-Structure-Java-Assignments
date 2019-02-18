51924f576dbe3db3c6eff86eb53fcf72e3b54783
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-02-18 10:18:32 -0500

    defined 2nd Recursive Method

--- Assignment03_COMP254/src/Plaindrome/Plaindrome.java
+++ Assignment03_COMP254/src/Plaindrome/Plaindrome.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 public class Plaindrome {
 
 	public static void main(String[] args) {
-		
-		Scanner reader= new Scanner (System.in);
+
+		Scanner reader = new Scanner(System.in);
 		System.out.print("Please enter a string : ");
-		String input=reader.nextLine();
+		String input = reader.nextLine();
+
+		System.out.println("Is a plaindrome : " + "  " + input + "  " + isPlaindrome(input));
+		System.out.print("Please center another string : ");
+		input=reader.nextLine();
 		
-		System.out.print("Is a plaindrome : " + input + isPlaindrome(input));
-		
-		
-		
-		
-		
+		System.out.println("Is a plaindrome : " + "  " + input + "  " + isPlaindrome(input));
+		reader.close();
 
 	}
-	
-	//Declaring a recursive boolean function
+
+	// Declaring a recursive boolean function
 	public static boolean isPlaindrome(String input) {
-		if (input==null ||input.isEmpty()) {
+		if (input == null || input.isEmpty()) {
 			return true;
 		}
-		char []array=input.toCharArray();
-		StringBuilder sb=new StringBuilder(input.length());
-		for (int i=input.length()-1; i>=0;i--) {
+		char[] array = input.toCharArray();
+		StringBuilder sb = new StringBuilder(input.length());
+		for (int i = input.length() - 1; i >= 0; i--) {
 			sb.append(array[i]);
 		}
-		String reverseOfString=sb.toString();
+		String reverseOfString = sb.toString();
 		return input.equals(reverseOfString);
-		
+
 	}
 
 }
