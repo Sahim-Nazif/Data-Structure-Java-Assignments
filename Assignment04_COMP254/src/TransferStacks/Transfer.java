66bb4bad94b3d99c1635cb8383b585cc79ee602d
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-03-09 19:06:25 -0500

    final commit

--- Assignment04_COMP254/src/TransferStacks/Transfer.java
+++ Assignment04_COMP254/src/TransferStacks/Transfer.java
@@ -2,6 +2,8 @@ package TransferStacks;
 
 import java.util.Stack;
 
+import java.util.Stack;
+
 public class Transfer {
 
     public static Stack<Integer> stackPush(Stack<Integer> stack){
@@ -15,4 +17,68 @@ public class Transfer {
         return stack;
 
     }
+
+    public static void pop(Stack<Integer> stack){
+
+        for(int i = 0; i < 5; i++){
+
+            Integer pop = (Integer) stack.pop();
+            System.out.println(pop);
+
+
+        }
+
+    }
+
+
+    public static void peek(Stack<Integer> stack){
+
+        Integer peek = (Integer) stack.peek();
+
+        System.out.println("Top of the element is: " + peek);
+
+    }
+
+
+    public static void search(Stack<Integer> stack, int element){
+
+        System.out.println("Element searched is: ");
+
+        Integer search = (Integer) stack.search(element);
+
+        if(element == -1){
+
+            System.out.println("Stack is empty.");
+        }
+        else{
+            System.out.println("Element is: " + search);
+        }
+
+
+    }
+
+    public static Stack<Integer> transfer(Stack<Integer> S_stack, Stack<Integer> T_stack){
+
+
+        T_stack = stackPush(S_stack);
+
+        System.out.println("Stack transfered successfully: "+ T_stack);
+
+        return T_stack;
+
+    }
+
+    public static void main(String[] args) {
+
+        Stack<Integer> S_stack = new Stack<>();
+        Stack<Integer> T_stack= new Stack<>();
+
+        Stack<Integer> Resultstack = transfer(S_stack, T_stack);
+
+        
+
+    }
+
+
+
 }
