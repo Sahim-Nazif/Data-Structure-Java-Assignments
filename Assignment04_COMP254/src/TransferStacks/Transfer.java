0f1f41062155fbb2ad98193f9b86f3be6af2669e
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-03-09 19:04:41 -0500

    method to push element

--- Assignment04_COMP254/src/TransferStacks/Transfer.java
+++ Assignment04_COMP254/src/TransferStacks/Transfer.java
@@ -1,5 +1,18 @@
 package TransferStacks;
 
+import java.util.Stack;
+
 public class Transfer {
 
+    public static Stack<Integer> stackPush(Stack<Integer> stack){
+
+        for(int i = 0; i < 5; i++){
+
+            Integer push = (Integer) stack.push(i);
+
+            System.out.println(push);
+        }
+        return stack;
+
+    }
 }
