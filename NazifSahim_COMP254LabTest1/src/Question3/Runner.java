eb0e7409d488c8859c1388f5652cbe15ade9ca39
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-03-14 19:49:37 -0400

    tested with push & pop methods

---
+++ NazifSahim_COMP254LabTest1/src/Question3/Runner.java
@@ -0,0 +1,24 @@
+package Question3;
+
+
+public class Runner { 
+	
+	
+public static void main(String[] args) 
+{ 
+  
+	HobbyLinkedlist hobby = new HobbyLinkedlist(); 
+   
+   hobby.push("Programming"); 
+   hobby.push("Reading"); 
+   hobby.push("Playing Cricket");
+
+ System.out.println("\nDislay my hobbies");
+ System.out.println("\n------------------");
+   hobby.display(); 
+   hobby.pop(); 
+   System.out.println("After pop ");
+   hobby.display(); 
+ 
+} 
+} 
