89eb80554eb6997635ba2944ce5fa028c7ce1e1a
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-03-29 19:20:35 -0400

    priority queue class created

--- Assignment05_COMP254/src/Exercise2/MinInHeap.java
+++ Assignment05_COMP254/src/Exercise2/MinInHeap.java
@@ -1,5 +1,125 @@
 package Exercise2;
 
+import java.util.Arrays;
+
 public class MinInHeap {
 
+	private int capacity = 10;
+	private int size = 0;
+
+	int[] items = new int[capacity];
+
+	private int getLeftChildIndex(int parentIndex) {
+		return 2 * parentIndex + 1;
+	}
+
+	private int getRightChildIndex(int parentIndex) {
+		return 2 * parentIndex + 2;
+	}
+
+	private int getParentIndex(int childIndex) {
+		return (childIndex - 1) / 2;
+	}
+
+	private boolean hasLeftChild(int index) {
+		return getLeftChildIndex(index) < size;
+	}
+
+	private boolean hasRightChild(int index) {
+		return getRightChildIndex(index) < size;
+	}
+
+	private boolean hasParent(int index) {
+		return getParentIndex(index) >= 0;
+	}
+
+	private int leftChild(int index) {
+		return items[getLeftChildIndex(index)];
+	}
+
+	private int RightChild(int index) {
+		return items[getRightChildIndex(index)];
+	}
+
+	private int parent(int index) {
+		return items[getParentIndex(index)];
+	}
+
+	private void swap(int indexOne, int indexTwo) {
+		int temp = items[indexOne];
+		items[indexOne] = items[indexTwo];
+		items[indexTwo] = temp;
+	}
+
+	private void ensureExtractCapacity() {
+		if (size == capacity) {
+			items = Arrays.copyOf(items, capacity * 2);
+			capacity *= 2;
+		}
+	}
+
+	public int peek() {
+		if (size == 0)
+			throw new IllegalStateException();
+		return items[0];
+
+	}
+
+	public int poll() {
+		if (size == 0)
+			throw new IllegalStateException();
+		int item = items[0];
+		items[0] = items[size - 1];
+		size--;
+		heapifyDown();
+		return item;
+	}
+
+	public void add(int item) {
+		ensureExtractCapacity();
+		items[size] = item;
+		size++;
+		heapifyUp();
+
+	}
+
+	public int remove() {
+		return items[--size];
+
+	}
+
+	public void printQueue() {
+		for (int i = 0; i < size; i++) {
+			System.out.println(items[i] + " ");
+		}
+	}
+
+	public void heapifyUp() {
+		int index = size - 1;
+		while (hasParent(index) && parent(index) > items[index]) {
+			swap(getParentIndex(index), index);
+			index = getParentIndex(index);
+
+		}
+	}
+
+	public void heapifyDown() {
+		int index = 0;
+		while (hasLeftChild(index)) {
+			int smallChildIndex = getLeftChildIndex(index);
+			if (hasRightChild(index) && RightChild(index) < leftChild(index)) {
+				smallChildIndex = getRightChildIndex(index);
+			}
+
+			if (items[index] < items[smallChildIndex]) {
+				break;
+
+			} else {
+				swap(index, smallChildIndex);
+
+			}
+			index = smallChildIndex;
+		}
+
+	}
 }
