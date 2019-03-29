0f592b1bc4665435d1e77d68d0a590c7c3a60ad9
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-03-29 19:43:28 -0400

    tested methods

--- Assignment05_COMP254/src/Exercise2/Tester.java
+++ Assignment05_COMP254/src/Exercise2/Tester.java
@@ -4,9 +4,16 @@ public class Tester {
 
 	public static void main(String[] args) {
     	MinInHeap minInHeap= new MinInHeap();
-    	System.out.println("Adding elements to the Priority Queue Array");
-    	System.out.println("-------------------------------------------");
+    	System.out.println("\nAdding elements to the Min Priority Queue Array");
+    	System.out.println("-----------------------------------------------");
     	minInHeap.add(10);
+    	minInHeap.add(15);
+		minInHeap.add(20);
+		minInHeap.add(17);
+		
+		minInHeap.printPriorityQueue();
+		System.out.println("Checking for the poll () function");
+		System.out.println("---------------------------------");
 
 		
 	}
