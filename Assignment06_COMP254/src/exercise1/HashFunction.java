520a96a429fbcb6cecf2f5844bbf7f26790d02d8
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-04-02 19:20:08 -0400

    created a class

---
+++ Assignment06_COMP254/src/exercise1/HashFunction.java
@@ -0,0 +1,12 @@
+package exercise1;
+//based on professor's instruction this assignment was implemented from an online resource
+
+public class HashFunction {
+	String [] theArray;
+	int arraySize;
+	int itemsInArray=0;
+	
+	
+	
+
+}
