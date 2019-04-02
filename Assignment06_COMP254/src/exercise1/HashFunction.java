d8aadf5b554a8574bf445f97fb94416dbdc21500
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-04-02 19:57:58 -0400

    added a method to print elements

--- Assignment06_COMP254/src/exercise1/HashFunction.java
+++ Assignment06_COMP254/src/exercise1/HashFunction.java
@@ -19,11 +19,6 @@ public class HashFunction {
 
 	}
 
-	public void displayStack() {
-		int increment = 0;
-
-	}
-
 	public void hashFunction1(String[] stringsForArray, String[] theArray) {
 		for (int n = 0; n < stringsForArray.length; n++) {
 			String newElementVal = stringsForArray[n];
@@ -31,4 +26,54 @@ public class HashFunction {
 		}
 	}
 
+	public void displayStack() {
+
+		int increment = 0;
+
+		for (int m = 0; m < 3; m++) {
+
+			increment += 10;
+
+			for (int n = 0; n < 71; n++)
+
+				System.out.print("-");
+
+			System.out.println();
+
+			for (int n = increment - 10; n < increment; n++) {
+
+				System.out.format("| %3s " + " ", n);
+
+			}
+
+			System.out.println("|");
+
+			for (int n = 0; n < 71; n++)
+
+				System.out.print("-");
+
+			System.out.println();
+
+			for (int n = increment - 10; n < increment; n++) {
+
+				if (theArray[n].equals("-1"))
+
+					System.out.print("|      ");
+
+				else
+
+					System.out.print(String.format("| %3s " + " ", theArray[n]));
+			}
+
+			System.out.println("|");
+
+			for (int n = 0; n < 71; n++)
+
+				System.out.print("-");
+
+			System.out.println();
+
+		}
+
+	}
 }
--- Assignment06_COMP254/src/exercise1/Tester.java
+++ Assignment06_COMP254/src/exercise1/Tester.java
@@ -3,6 +3,12 @@ package exercise1;
 public class Tester {
 
 	public static void main(String[] args) {
+		
+		HashFunction func= new HashFunction(30);
+		
+		String []elementsToAddStrings= {"1", "5","17","21","26"};
+		func.hashFunction1(elementsToAddStrings, func.theArray);
+		func.displayStack();
 
 	}
 
