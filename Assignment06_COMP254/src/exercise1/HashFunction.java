e6cc38eb9722eb6c0764f36998e67cf69c806b79
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-04-02 19:42:25 -0400

    added hashFunction

--- Assignment06_COMP254/src/exercise1/HashFunction.java
+++ Assignment06_COMP254/src/exercise1/HashFunction.java
@@ -1,12 +1,35 @@
 package exercise1;
+
+import java.util.Arrays;
+
 //based on professor's instruction this assignment was implemented from an online resource
 
 public class HashFunction {
+	//creating array with size and elements in the array
 	String [] theArray;
 	int arraySize;
 	int itemsInArray=0;
 	
+	//creating a constructor
 	
+	public HashFunction(int size) {
+		arraySize=size;
+		theArray= new String[size];
+		Arrays.fill(theArray, "-1");
+				
+	}
+	
+	public void displayStack() {
+		int increment=0;
+		
+	}
+	
+	public void hashFunction1(String [] stringsForArray, String[]theArray) {
+		for (int n=0; n<stringsForArray.length;n++) {
+			String newElementVal=stringsForArray[n];
+			theArray[Integer.parseInt(newElementVal)]=newElementVal;
+		}
+	}
 	
 
 }
