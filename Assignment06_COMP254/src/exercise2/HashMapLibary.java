f4a1c1cefc3c033d63d4dcfa224a34c500613d25
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-04-02 20:58:49 -0400

    stored data in hashmap

--- Assignment06_COMP254/src/exercise2/HashMapLibary.java
+++ Assignment06_COMP254/src/exercise2/HashMapLibary.java
@@ -7,9 +7,14 @@ public class HashMapLibary {
 
 	public static void main(String[] args) {
 
-      Map <String , Integer> hashmap=new HashMap<String, Integer>();
-      
-      
+		Map<String, Integer> hashmap = new HashMap<String, Integer>();
+		
+		hashmap.put("Java Early Object", 75);
+		hashmap.put("Android Programming Book", 44);
+		hashmap.put("Asp.Net MVC", 85);
+		hashmap.put("Python Book", 55);
+		
+		
 
 	}
 
