0a615ac15d5de2238dc923caf60a488dee9042b4
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-04-02 20:50:10 -0400

    created Hashmap Class

---
+++ Assignment06_COMP254/src/exercise2/HashMapLibary.java
@@ -0,0 +1,16 @@
+package exercise2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapLibary {
+
+	public static void main(String[] args) {
+
+      Map <String , Integer> hashmap=new HashMap<String, Integer>();
+      
+      
+
+	}
+
+}
