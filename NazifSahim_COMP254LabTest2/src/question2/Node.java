473e34d6e839bc225b4ebc611884860eb707af97
Sahim Nazif <nsahim@my.centennialcollege.ca>
2019-04-17 19:59:40 -0400

    final commit

--- NazifSahim_COMP254LabTest2/src/question2/Node.java
+++ NazifSahim_COMP254LabTest2/src/question2/Node.java
@@ -2,47 +2,48 @@ package question2;
 
 public class Node {
 
-	//this class has pointers to left and right nodes
+	// this class has pointers to left and right nodes
 	Node left, right;
-	//data to insert
+	// data to insert
 	String data;
-	//constructor
-public Node(String data) {
-	this.data=data;
-	
-}
+
+	// constructor
+	public Node(String data) {
+		this.data = data;
+
+	}
 //this method will insert value to the node
 
-public void insert(String value) {
-	//first check if value is less than data (root)
-	//if (value<=data) {
-		//first check if left is null, then left will be our new Node
-		if(left==null) {
-			left=new Node(value);
-			
-		}else {
+	public void insert(String value) {
+		
+		// first check if left is null, then left will be our new Node
+		if (left == null) {
+			left = new Node(value);
+
+		} else {
 			left.insert(value);
 		}
-}
-public void printPreOrder() {
-	
-	System.out.println(data);
-	if (left!=null) {
-		left.printPreOrder();
 	}
-	if (right !=null) {
-		right.printPreOrder();
-	}
-}
 
-public void printPostOrder() {
-	if (left!=null) {
-		left.printPostOrder();
+	public void printPreOrder() {
+
+		System.out.println(data);
+		if (left != null) {
+			left.printPreOrder();
+		}
+		if (right != null) {
+			right.printPreOrder();
+		}
 	}
-	if (right !=null) {
-		right.printPostOrder();
+
+	public void printPostOrder() {
+		if (left != null) {
+			left.printPostOrder();
+		}
+		if (right != null) {
+			right.printPostOrder();
+		}
+		System.out.println(data);
 	}
-	System.out.println(data);
-}
 
 }
--- NazifSahim_COMP254LabTest2/src/question2/Tester.java
+++ NazifSahim_COMP254LabTest2/src/question2/Tester.java
@@ -4,21 +4,16 @@ public class Tester {
 
 	public static void main(String[] args) {
 
-    
-		Node node=new Node("COMP 254");
-				
+		Node node = new Node("COMP 254");
+
 		node.insert("COMP 304");
-		node.insert("COMP 301");
-		
+		node.insert("COMP 222");
+
 		System.out.println("Print all the elements");
 		node.printPreOrder();
-        System.out.println("This is the post order method");
-        node.printPostOrder();
-        
-     
-        
-        
-        
+		System.out.println("This is the post order method");
+		node.printPostOrder();
+
 	}
 
 }
